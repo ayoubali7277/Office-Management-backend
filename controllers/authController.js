3911d3c0d275e154929454845960af613dfbf400
Wahab <info.fastengineers@gmail.com>
2026-09-24 00:40:35 +0500

    Fix production authentication cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,12 +66,12 @@ export const login = async (req, res) => {
     }
   );
 
-  res.cookie("token", token, {
+ res.cookie("token", token, {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
-  maxAge: 2 * 60 * 60 * 1000,});
-
+  secure: true,
+  sameSite: "none",
+  maxAge: 2 * 60 * 60 * 1000,
+});
   return res.status(200).json({
     message: "Login successful",
     token,
