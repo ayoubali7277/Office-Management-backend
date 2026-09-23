416de271a718e54bc194434848f5b991fa8ccb62
Wahab <info.fastengineers@gmail.com>
2026-09-24 00:16:16 +0500

    Fix production CORS origin

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin:"https://office-management-frontend-production.up.railway.app",
     credentials: true,
   })
 );
