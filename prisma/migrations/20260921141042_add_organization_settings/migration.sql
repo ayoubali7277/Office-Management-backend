9972b8fd31e6ca93424852f30374560f6f523b96
ayoubali7277 <ayoub.aliofficial@gmail.com>
2026-09-23 22:42:02 +0500

    Update migration.sql

--- prisma/migrations/20260921141042_add_organization_settings/migration.sql
+++ prisma/migrations/20260921141042_add_organization_settings/migration.sql
@@ -1,3 +1,3 @@
 -- AlterTable
-ALTER TABLE `organization` ADD COLUMN `address` VARCHAR(191) NULL,
+ALTER TABLE `Organization` ADD COLUMN `address` VARCHAR(191) NULL,
     ADD COLUMN `timezone` VARCHAR(191) NULL;
